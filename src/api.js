6e7effc3c17478507303bcac094730036a420c50
wlss <wlss@MacBook-Air-de-Wagner.local>
2026-04-07 19:13:02 -0300

    atualizacoes frontend - filtros, login, layout mobile

--- src/api.js
+++ src/api.js
@@ -1,7 +1,8 @@
 import axios from 'axios'
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || 'http://localhost:8000',
+  //baseURL: import.meta.env.VITE_API_URL || 'http://localhost:8000',
+  baseURL: 'https://elite-volleyball-backend.onrender.com',
 })
 
 export const listarJogadores  = () => api.get('/jogadores').then(r => r.data)
